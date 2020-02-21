5b80ce52ad4aeb9f7122e758607ea0431543ddc6
lzjyzq2 <1462678018@qq.com>
2020-02-21 18:02:43 +0800

    refactor(test):重构test内文件，使用sblog-dev中的test配置文件

--- sblog/src/test/java/cn/settile/sblog/SblogApplicationTests.java
+++ sblog/src/test/java/cn/settile/sblog/SblogApplicationTests.java
@@ -1,16 +1,22 @@
 package cn.settile.sblog;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SblogApplication.class)
+@TestPropertySource(locations ="file:./sblog-dev/application-test.properties")
 public class SblogApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
