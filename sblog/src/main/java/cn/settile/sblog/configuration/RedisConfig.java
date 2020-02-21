a6616b19d98328b0f28fcd7f3924be7adc885802
lzjyzq2 <1462678018@qq.com>
2020-02-21 18:06:42 +0800

    style(cache):优化redis缓存配置

--- sblog/src/main/java/cn/settile/sblog/configuration/RedisConfig.java
+++ sblog/src/main/java/cn/settile/sblog/configuration/RedisConfig.java
@@ -1,9 +1,12 @@
 package cn.settile.sblog.configuration;
 
+import cn.settile.sblog.controller.api.RegisterController;
+import cn.settile.sblog.filter.aspect.AccessLimitAspect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
@@ -26,10 +29,10 @@ import static java.util.Collections.singletonMap;
 
 /**
  * @author : lzjyz
- * @date : 2020-02-02 15:10
+ * @date 2020-02-21 16:52:24
  */
 @Configuration
-@EnableCaching // 开启缓存支持
+@EnableCaching
 public class RedisConfig extends CachingConfigurerSupport {
     @Resource
     private LettuceConnectionFactory lettuceConnectionFactory;
@@ -37,7 +40,7 @@ public class RedisConfig extends CachingConfigurerSupport {
     /**
      * 自定义策略生成的key
      * 自定义的缓存key的生成策略 若想使用这个key
-     * 只需要讲注解上keyGenerator的值设置为keyGenerator即可</br>
+     * 只需要讲注解上keyGenerator的值设置为keyGenerator即可
      */
     @Override
     @Bean
@@ -63,16 +66,20 @@ public class RedisConfig extends CachingConfigurerSupport {
         Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, Visibility.ANY);
-        om.enableDefaultTyping(DefaultTyping.NON_FINAL);
+        om.activateDefaultTyping(om.getPolymorphicTypeValidator(),DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
         // 配置redisTemplate
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
         redisTemplate.setConnectionFactory(lettuceConnectionFactory);
         RedisSerializer<?> stringSerializer = new StringRedisSerializer();
-        redisTemplate.setKeySerializer(stringSerializer);// key序列化
-        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);// value序列化
-        redisTemplate.setHashKeySerializer(stringSerializer);// Hash key序列化
-        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);// Hash value序列化
+        // key序列化
+        redisTemplate.setKeySerializer(stringSerializer);
+        // value序列化
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        // Hash key序列化
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        // Hash value序列化
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
@@ -87,19 +94,10 @@ public class RedisConfig extends CachingConfigurerSupport {
         RedisCacheConfiguration redisCacheConfiguration = config.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
 
-
-        // 以锁写入的方式创建RedisCacheWriter对象
-        //RedisCacheWriter writer = RedisCacheWriter.lockingRedisCacheWriter(factory);
-        // 创建默认缓存配置对象
-        /* 默认配置，设置缓存有效期 1小时*/
-        //RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(1));
-
-        RedisCacheManager cacheManager = RedisCacheManager.builder(
-                RedisCacheWriter
-                        .lockingRedisCacheWriter(factory))
+        return RedisCacheManager.builder(RedisCacheWriter.lockingRedisCacheWriter(factory))
                 .cacheDefaults(redisCacheConfiguration)
-        //增加新的缓存分组        .withInitialCacheConfigurations(singletonMap("test", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(120)).disableCachingNullValues()))
+                .withInitialCacheConfigurations(singletonMap(RegisterController.REG_CODE_CACHENAME, redisCacheConfiguration.entryTtl(Duration.ofSeconds(60)).disableCachingNullValues()))
+                .withInitialCacheConfigurations(singletonMap(AccessLimitAspect.IP_LIMIT_CACHENAME, redisCacheConfiguration.entryTtl(Duration.ofSeconds(10)).disableCachingNullValues()))
                 .transactionAware().build();
-        return cacheManager;
     }
 }
