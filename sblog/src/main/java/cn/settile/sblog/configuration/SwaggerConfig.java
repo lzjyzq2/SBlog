e23f85b706c7f5ba8bfef6b6b4a23f4ab394972b
lzjyzq2 <lzjyzq2@qq.com>
2020-05-11 23:32:38 +0800

    fix(config):修正Swagger携带Token无效的问题

--- sblog/src/main/java/cn/settile/sblog/configuration/SwaggerConfig.java
+++ sblog/src/main/java/cn/settile/sblog/configuration/SwaggerConfig.java
@@ -8,10 +8,14 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,9 +32,26 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("cn.settile.sblog.controller.api"))  // 注意修改此处的包名
                 .paths(PathSelectors.any())
                 .build()
-                .securitySchemes(List.of(apiKey()));
+                .securitySchemes(List.of(apiKey()))
+                .securityContexts(securityContexts());
     }
-
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        List<SecurityReference> securityReferences=new ArrayList<>();
+        securityReferences.add(new SecurityReference("Authorization", authorizationScopes));
+        return securityReferences;
+    }
+    private List<SecurityContext> securityContexts() {
+        List<SecurityContext> securityContexts=new ArrayList<>();
+        securityContexts.add(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+//                        .forPaths(PathSelectors.regex("正则"))
+                        .build());
+        return securityContexts;
+    }
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("SBlog-支持单用户/多用户的博客系统")
@@ -40,6 +61,6 @@ public class SwaggerConfig {
     }
 
     private ApiKey apiKey() {
-        return new ApiKey("apikey", CommonConstant.ACCESS_TOKEN, "header");
+        return new ApiKey("Authorization", CommonConstant.ACCESS_TOKEN, "header");
     }
 }
