80e9160ba7f91f5f3e1fec7e04a9e3f0aeb478e9
lzjyzq2 <lzjyzq2@qq.com>
2020-05-15 14:12:20 +0800

    fix(handler):增加对Filter抛出异常的处理

--- sblog/src/main/java/cn/settile/sblog/handler/ServiceExceptionHandler.java
+++ sblog/src/main/java/cn/settile/sblog/handler/ServiceExceptionHandler.java
@@ -6,25 +6,50 @@ import cn.settile.sblog.exception.UnAuthenticationException;
 import cn.settile.sblog.exception.result.Result;
 import cn.settile.sblog.service.ThemeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authz.AuthorizationException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 /**
  * @author : lzjyz
  * @date : 2020-01-21 14:01
  */
-@ControllerAdvice
 @Slf4j
-public class ServiceExceptionHandler {
+@RestControllerAdvice
+@Controller
+public class ServiceExceptionHandler implements ErrorController {
     @Autowired
     ThemeService themeService;
 
+    private static final String PATH_DEFAULT = "/error";
+
     @ExceptionHandler(UnAuthenticationException.class)
-    @ResponseBody
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public Result handleUnAuthorize(UnAuthenticationException e) {
+        log.error("验证失败");
         return e.getResult();
+    }
+
+    @RequestMapping(PATH_DEFAULT)
+    public void handleError(HttpServletRequest request) throws Throwable {
+        if (request.getAttribute("javax.servlet.error.exception") != null) {
+            throw (Throwable) request.getAttribute("javax.servlet.error.exception");
+        }
     }
 
     @ExceptionHandler(NoFoundException.class)
@@ -33,8 +58,16 @@ public class ServiceExceptionHandler {
     }
 
     @ExceptionHandler(ServiceException.class)
-    @ResponseBody
+    @ResponseBody @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Result handleServiceException(ServiceException e) {
+        log.error("系统异常");
         return e.getResult();
+    }
+
+
+
+    @Override
+    public String getErrorPath() {
+        return PATH_DEFAULT;
     }
 }
